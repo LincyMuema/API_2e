8d75e49af237b20f7f876f34213beffd3b3f8849
LincyMuema <lincy.muema@strathmore.edu>
2024-10-24 20:13:50 +0300

    adding verifications

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,21 @@
 <?php
+date_default_timezone_set("AFRICA/Nairobi");
 define('DBTYPE','PDO');
 define('HOSTNAME','localhost');
 define('DBPORT','3308');
 define('HOSTUSER','root');
 define('HOSTPASS','1@3mys0lwork');
 define('DBNAME','2E_ics');
+
+$protocol = isset($_SERVER['HTTPS']) && 
+$_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+$base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
+
+$conf['ver_code_time'] = date("Y-m-d H:i:s", strtotime("+ 1 days"));
+$conf['verification_code'] = rand(10000,99999);
+$conf['site_initials'] = "ICS 2024";
+$conf['site_url'] = "$base_url/". DBNAME;
+
+
+// verify that the email domain
+$conf['valid_domains'] = ["strathmore.edu", "gmail.com", "yahoo.com", "mada.co.ke", "outlook.com"];
